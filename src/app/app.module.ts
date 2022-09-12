ce5e97a397cba2b2eeda294dd609fb6e38188a9c
duongpm1 <duongpm@foobla.com>
2022-09-12 19:59:42 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppComponent, ChecklistDatabase } from './app.component';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTreeModule,
+    MatCheckboxModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [ChecklistDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
